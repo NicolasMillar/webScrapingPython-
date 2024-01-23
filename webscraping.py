f1bde405b68b2a20458a64587f5ecf7cf7006b86
Nicolas <nicolas.millar.millar@gmail.com>
2024-01-23 17:39:32 -0300

    continuando con la reestructuracion

--- webscraping.py
+++ webscraping.py
@@ -1,7 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
 
-def getCardsInfo(box):
+def getBox(url):
+    website = url
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+    result = requests.get(website, headers=headers)
+    content = result.text
+
+    soup = BeautifulSoup(content, 'lxml')
+    box = soup.find('article', class_='col-lg-9')
+
+    return box
+
+def getCardsInfo(box, booster):
     cards_list = []
     if box:
         cards = box.find_all('div', class_='bs-product')
@@ -13,27 +24,22 @@ def getCardsInfo(box):
             card_info = {
                 'name' : card_name,
                 'img' : card_img,
+                'booster' : booster
             }
             
             cards_list.append(card_info)
 
     return cards_list
 
-baseUrl = 'https://www.guildreams.com'
-website = 'https://www.guildreams.com/collection/digimon-bt1-release-special?limit=24&with_stock=0&smart_stock=0&order=name&way=ASC'
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
-result = requests.get(website, headers=headers)
-content = result.text
 
-soup = BeautifulSoup(content, 'lxml')
-box = soup.find('article', class_='col-lg-9')
-
-cards_list = getCardsInfo(box)
+box = getBox('https://www.guildreams.com/collection/digimon-bt1-release-special?limit=24&with_stock=0&smart_stock=0&order=name&way=ASC&&page=1')
+cards_list = getCardsInfo(box, 'bt1')
 
 if cards_list:
     print("Información guardada:")
     for card in cards_list:
         print(f"Nombre: {card['name']}")
         print(f"Imagen: {card['img']}")
+        print(f"Booster: {card['booster']}")
 else:
     print("No se encontró información para guardar.")
