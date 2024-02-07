f51323be589b825b8977608ee2c14503b0a4b3be
Nicolas <nicolas.millar.millar@gmail.com>
2024-02-06 21:26:22 -0300

    ocultando el url de la api

--- guilddream.py
+++ guilddream.py
@@ -36,7 +36,7 @@ def getCardsList(limit, Baseurl, booster, connection):
         getCardsInfo(box, booster, connection)
 
 def getDataApi(name, booster):
-    api_url = "http://127.0.0.1:5000/cards"
+    api_url = os.getenv('api_url')
     params = {'cardName': name, 'booster': booster}
 
     try:
