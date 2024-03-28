6cc9a2128f6efe83a5e7e22c4eb6165c9766142b
Nicolas <nicolas.millar.millar@gmail.com>
2024-03-28 15:33:31 -0300

    correccion al obtener el nombre

--- deckcards.py
+++ deckcards.py
@@ -6,24 +6,28 @@ from common import getBox,getDataApi, getCardId, insertData
 load_dotenv()
 baseUrl = 'https://www.deckscards.cl/'
 
-def getCardsList(limit, url, booster):
+def getCardsList(limit, url, booster, cards):
     for page in range(1, limit+1):
         pageUrl = f'{url}{page}'
         box = getBox(pageUrl, 'div', 'box-product row')
-        getCardsInfo(box)
+        getCardsInfo(box, cards)
         print(f"pagina: {page} del booster: {booster} completada")
 
-def getCardsInfo(box):
+def getCardsInfo(box, cardsDb):
     if box:
         cards = box.find_all('div', class_='col-md-4 col-xs-6')
 
         for card in cards:
             aux = card.find('h3', class_= 'product-name').a
             card_name = aux.text.strip()
+            unwanted_words = ["Digimon", "Option", "Digi-Egg", "Tamer"]
+            for word in unwanted_words:
+                card_name = card_name.replace(word, '')
+
             card_url = baseUrl + aux['href']
             card_price = card.find('span', class_= 'final_price').text.strip().replace('CLP', '')
             
-            print(card_price)
+            print(card_name)
 
 try:
     connection = psycopg2.connect(
@@ -36,7 +40,7 @@ try:
 
     print("Conexion realizada con exito")
     cards = getDataApi()
-    getCardsList(3, 'https://www.deckscards.cl/digimon/booster-ver10?sorting=name-asc&page=', 'bt1')
+    getCardsList(3, 'https://www.deckscards.cl/digimon/booster-ver10?sorting=name-asc&page=', 'bt1', cards)
 
 except Exception as e:
     print(f"Error de conexión: {e}")
